41024620768d33eff371f3ac262d36e551b5d84a
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2020-11-06 13:43:01 +0000

    Writting test for strings helpers

---
+++ tests/StringsHelperTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drewlabs\Core\Helpers\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class StringsHelperTest extends TestCase
+{
+
+    public function testStringAfterFunction()
+    {
+        $haystack = 'DonalTrumpVsJoeBaiden';
+
+        $this->assertEquals(\drewlabs_core_strings_after('Vs', $haystack), 'DonalTrump', 'Expect strings afert Vs to be JoeBaiden');
+    }
+}
